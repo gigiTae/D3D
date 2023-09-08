b3ab1fd36ba1dc8ff2a291ac9488c3dcf848c5df
GiTae <rlxo800@gmail.com>
2023-09-08 22:04:33 +0900

    기초작업

--- Direct3D/App/WindowsApp.cpp
+++ Direct3D/App/WindowsApp.cpp
@@ -1,8 +1,5 @@
 ﻿// App.cpp : 애플리케이션에 대한 진입점을 정의합니다.
-
 #include "pch.h"
-#include "framework.h"
-#include "App.h"
 
 #define MAX_LOADSTRING 100
 
@@ -22,12 +19,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_ LPWSTR    lpCmdLine,
                      _In_ int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
-
-    // TODO: 여기에 코드를 입력합니다.
-    int a = 0;
-
+   
     // 전역 문자열을 초기화합니다.
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_APP, szWindowClass, MAX_LOADSTRING);
--- Direct3D/App/pch.h
+++ Direct3D/App/pch.h
@@ -8,6 +8,24 @@
 #define PCH_H
 
 // 여기에 미리 컴파일하려는 헤더 추가
-#include "framework.h"
+
+// // SDKDDKVer.h를 포함하면 최고 수준의 가용성을 가진 Windows 플랫폼이 정의됩니다.
+// 이전 Windows 플랫폼용 애플리케이션을 빌드하려는 경우에는 SDKDDKVer.h를 포함하기 전에
+// WinSDKVer.h를 포함하고 _WIN32_WINNT를 지원하려는 플랫폼으로 설정합니다.
+#include <SDKDDKVer.h>
+
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+// Windows 헤더 파일
+#include <windows.h>
+// C 런타임 헤더 파일입니다.
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#include "EngineModule/EngineHeader.h"
+
+
+#include "resource.h"
 
 #endif //PCH_H
